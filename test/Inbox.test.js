56a26a1e427dfaff2a847c9205543d8adc38f94e
Zachery Gentry <zacherygentry@gmail.com>
2021-08-09 21:28:55 -0500

    added gitignore

---
+++ test/Inbox.test.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+const defaultMessage = 'Howdy there';
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // Use one of those accounts to deploy the contract
+
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: [defaultMessage],
+    })
+    .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, defaultMessage);
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods
+      .setMessage('Well hello there')
+      .send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, 'Well hello there');
+  });
+});
